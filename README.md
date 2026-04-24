203bd99df8780f8006b4e035d9e5aad8c81ba116
Vivian Dai <38384400+vivian-dai@users.noreply.github.com>
2026-04-23 21:18:56 -0400

    this thing

---
+++ README.md
@@ -0,0 +1,9 @@
+# RandomDumpOfThingsToHost
+
+## Description
+Just like [RandomDumpOfSmallStupidProjects](https://github.com/vivian-dai/RandomDumpOfSmallStupidProjects/) but static websites
+
+## Projects
+|Name|Description|
+|-|-|
+|[Print Pauser](./filament_change_pause/)|upload a GCode file, give it a layer to pause at for filament swaps and it gives you a new one back|
---
+++ filament_change_pause/script.js
@@ -0,0 +1,46 @@
+const PAUSE_CMD = "M600"; // hopefully it's supported otherwise womp womp
+const LAYER_IND = ";Layer height:";
+
+document.getElementById("ok-btn").addEventListener("click", addPause)
+
+function addPause() {
+    const fIn = document.getElementById("in-file").files[0];
+
+    const reader = new FileReader();
+    reader.onload = function(event) {
+        let content = event.target.result;
+
+        let layerSize = -1;
+
+        let lines = content.split("\n");
+        for (let i = 0; i < lines.length; i++) {
+            if(lines[i].includes(LAYER_IND)) {
+                layerSize = Number(lines[i].split(LAYER_IND)[1].trim());
+            }
+            
+        }
+        if(layerSize != -1) {
+            const z_off = layerSize * document.getElementById("layer").value;
+            let newContent = [];
+            for (let i = 0; i < lines.length; i++) {
+                const reg = new RegExp(`G0\\b.*\\bZ${z_off}\\b`);
+                if(reg.test(lines[i])) {
+                    newContent.push(PAUSE_CMD);
+                }
+                newContent.push(lines[i]);
+            }
+
+            const blob = new Blob([newContent.join("\n")], { type: "text/plain" });
+
+            const url = URL.createObjectURL(blob);
+            const link = document.getElementById("download");
+
+            link.href = url;
+            link.download = "new_and_improved" + fIn.name;
+            link.style.display = "inline";
+            link.textContent = "Download Processed File";
+        }
+    };
+
+    reader.readAsText(fIn);
+}
